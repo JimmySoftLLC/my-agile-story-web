f31ade848cfb5e7653ee8c71d2717e22170f6b52
James Bailey <16010577+JimmySoftLLC@users.noreply.github.com>
2019-09-12 23:10:26 -0400

    First provisioned beta

--- js/restAPIs.js
+++ js/restAPIs.js
@@ -1,4 +1,5 @@
-const URL_Address = 'http://127.0.0.1:3004';
+//const URL_Address = 'http://127.0.0.1:3004';
+const URL_Address = 'https://shrouded-basin-24147.herokuapp.com';
 
 function loginDeveloper() {
     var email = document.getElementById('login-email').value;
